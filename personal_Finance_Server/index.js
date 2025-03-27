c9bbf9829f33ad2f944cdf2eef01172c4202b5ca
murph98ma <mitchellmurphy147@gmail.com>
2025-03-27 00:43:33 -0400

    Updated some npm packages. Got the growth graph to pull from the backend.

--- personal_Finance_Server/index.js
+++ personal_Finance_Server/index.js
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = 5000;
+app.use(cors());
+
 
 
 app.get("/growthTable", (req, res) => {
     res.json({
-        months: ["Jan", "Feb", "March"],
-        values: [2, 45, 3]
+        months: ["Jan", "Feb", "March", "April","May", "June"],
+        values: [2, 45, 3, 100]
     });
 });
 
