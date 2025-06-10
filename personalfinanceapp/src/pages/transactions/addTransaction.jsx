b7a3e78363337c7424821bd772ba9430d74267a6
murph98ma <mitchellmurphy147@gmail.com>
2025-06-10 17:58:23 -0500

    Added checkboxes to select what type of transaction the user will be adding.

--- personalfinanceapp/src/pages/transactions/addTransaction.jsx
+++ personalfinanceapp/src/pages/transactions/addTransaction.jsx
@@ -2,12 +2,45 @@ import React, {useState} from "react";
 import "./addTransaction.css"
 
 const AddTransaction = ({onClose}) => {
+    const [transactionTypeSelection, setTransactionTypeSelection] = useState(null);
+    const handleTransactionTypeCheck = (type) => {
+        setTransactionTypeSelection(prev => (prev === type ? null : type));
+    };
 
     return(
      <div className="overlay">
         <div className="content">
-            <h3>Pop-up is showing!</h3>
             <button onClick={onClose}>X</button>
+            <div>
+                <label>
+                    <input
+                        type ="checkbox"
+                        checked = {transactionTypeSelection === 'Deposit'}
+                        onChange={() => handleTransactionTypeCheck('Deposit')}
+                    />
+                    Show Deposit Form
+                </label>
+                <label>
+                    <input
+                        type = "checkbox"
+                        checked={transactionTypeSelection === "Debit"}
+                        onChange={() => handleTransactionTypeCheck('Debit')}
+                    />
+                    Show Debit Form
+                </label>
+            </div>
+            <div>
+                {transactionTypeSelection === 'Deposit' &&(
+                    <div>
+                        <h3>Deposit Form is showing!!!</h3>
+                    </div> 
+                )}
+                {transactionTypeSelection === 'Debit' && (
+                    <div> 
+                        <h3>DEBIT FORM IS SHOWING</h3>
+                    </div>
+                )}
+            </div>
         </div> 
     </div>
     );  
